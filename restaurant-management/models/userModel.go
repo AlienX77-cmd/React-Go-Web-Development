875f4fb9cd587e3b1293ba16880f3405d8662be4
Kittipak <67796659+AlienX77-cmd@users.noreply.github.com>
2025-01-10 14:07:01 +0700

    Update userModel.go

--- restaurant-management/models/userModel.go
+++ restaurant-management/models/userModel.go
@@ -7,7 +7,7 @@ import (
 )
 
 type User struct {
-	ID         				primitive.ObjectID 		`bson:"_id"`
+	ID         				primitive.ObjectID 			`bson:"_id"`
 	First_name 				*string					`json:"first_name" validate:"required,min=2,max=100"` 
 	Last_name				*string					`json:"last_name" validate:"required,min=2,max=100"`
 	Password				*string					`json:"Password" validate:"required,min=6"`
@@ -15,7 +15,7 @@ type User struct {
 	Avatar					*string					`json:"avatar"`
 	Phone					*string					`json:"phone" validate:"required"`
 	Token					*string					`json:"token"`
-	Refresh_Token			*string					`json:"refresh_token"`
+	Refresh_Token				*string					`json:"refresh_token"`
 	Created_at				time.Time				`json:"created_at"`
 	Updated_at				time.Time				`json:"updated_at"`
 	User_id					string					`json:"user_id"`
